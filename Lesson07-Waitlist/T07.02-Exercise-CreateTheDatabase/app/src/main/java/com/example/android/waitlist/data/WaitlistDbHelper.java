50d2fb1764cd6c8f9dfe8e8cab5f9486f0362fb1
201511045 <yjflore34@gmail.com>
2018-02-27 12:36:16 +0900

    exerciseT07.03

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,22 +1,51 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.waitlist.data.WaitlistContract.WaitlistEntry;
+
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+  // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+  private static final String DATABASE_NAME = "waitlist.db";
+  // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+  private static final int DATABASE_VERSION = 1;
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+  // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+  public WaitlistDbHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
 
-    // TODO (5) Override the onCreate method
 
-        // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+  // TODO (5) Override the onCreate method
+  // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+  // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+  @Override
+  public void onCreate(SQLiteDatabase db) {
+    final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE"
+        + WaitlistEntry.TABLE_NAME + "(" +
+        WaitlistEntry._ID +"INTEGER PRIMARY KEY AUTOINCREMENT,"
+        +WaitlistEntry.COLUMN_GUEST_NAME + "TEXT NOT NULL,"
+        +WaitlistEntry.COLUMN_PARTY_SIZE + "INTEGER NOT NULL,"
+        +WaitlistEntry.COLUMN_PARTY_SIZE +" TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+        +");";
+    db.execSQL(SQL_CREATE_WAITLIST_TABLE);
 
-        // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+  }
 
-    // TODO (8) Override the onUpgrade method
+  // TODO (8) Override the onUpgrade method
 
-        // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+  // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME);
+    onCreate(db);
+  }
+
 
 }
--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistContract.WaitlistEntry;
+import com.example.android.waitlist.data.WaitlistDbHelper;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
 
     // TODO (1) Create a local field member of type SQLiteDatabase called mDb
-
+    private SQLiteDatabase mDB ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,19 +31,18 @@ public class MainActivity extends AppCompatActivity {
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this);
 
         // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
-
+        WaitlistDbHelper waitlistDbHelper = new WaitlistDbHelper(this);
         // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
-
+        mDB = waitlistDbHelper.getWritableDatabase();
         // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
-
+        TestUtil.insertFakeData(mDB);
         // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
-
+        Cursor cursor = getAllGuests(mDB);
         // TODO (12) Pass the resulting cursor count to the adapter
-
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this,cursor.getCount());
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
 
@@ -54,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (5) Create a private method called getAllGuests that returns a cursor
-
     // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+    private Cursor getAllGuests(SQLiteDatabase mDB) {
+    return mDB.query(WaitlistEntry.TABLE_NAME,
+        null,
+        null,
+        null,
+        null,
+        null,
+        WaitlistEntry.COLUMN_TIMESTAMP);
+    }
 
 }
